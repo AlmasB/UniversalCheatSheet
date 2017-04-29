145fa01979dce38b75f6ff8df79b529a32d41f2a
AlmasB <almaslvl@gmail.com>
2017-04-29 12:41:25 +0100

    fixed var init

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -10,7 +10,7 @@ mvn versions:commit
 git add pom.xml
 git add */pom.xml
 
-commit_msg = "bump_version.sh $1-SNAPSHOT"
+commit_msg="bump_version.sh $1-SNAPSHOT"
 
 git commit -m "\"$commit_msg\""
 git push --set-upstream origin $branch_name
