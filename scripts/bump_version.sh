3da98709199191525acce4a73ec64135e65070ec
AlmasB <almaslvl@gmail.com>
2017-04-29 12:36:10 +0100

    fixed var name

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -2,12 +2,13 @@
 
 branch_name="$1"
 
-git checkout -b branch_name
+git checkout -b $branch_name
 
 mvn versions:set -DnewVersion="$1-SNAPSHOT"
 mvn versions:commit
 
-git add pom.xml */pom.xml
+git add pom.xml
+git add */pom.xml
 git commit -m 'bump_version.sh $1-SNAPSHOT'
 git push --set-upstream origin $branch_name
 
