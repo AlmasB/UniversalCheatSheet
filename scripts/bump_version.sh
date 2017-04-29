13d457624d65756da9d405efac82fe3c5cb204cb
AlmasB <almaslvl@gmail.com>
2017-04-29 12:30:40 +0100

    testing autobump

---
+++ scripts/bump_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+branch_name="$1"
+
+git checkout -b branch_name
+
+mvn versions:set -DnewVersion="$1-SNAPSHOT"
+mvn versions:commit
+
+git add pom.xml */pom.xml
+git commit -m 'bump_version.sh $1-SNAPSHOT'
+git push --set-upstream origin $branch_name
+
+echo "Bumped to $branch_name"
