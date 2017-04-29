b82f44aba7ff2393078da199a47d5b0ecf4a1286
AlmasB <almaslvl@gmail.com>
2017-04-29 12:38:33 +0100

    fixing commit message

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -9,7 +9,7 @@ mvn versions:commit
 
 git add pom.xml
 git add */pom.xml
-git commit -m 'bump_version.sh $1-SNAPSHOT'
+git commit -m 'bump_version.sh "$1"-SNAPSHOT'
 git push --set-upstream origin $branch_name
 
 echo "Bumped to $branch_name"
