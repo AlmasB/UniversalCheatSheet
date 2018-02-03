4d65e1b90aada5f255376e479e916a8adf4ee4d7
AlmasB <almaslvl@gmail.com>
2018-02-03 19:25:07 +0000

    added release script

---
+++ scripts/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# assume running on master branch
+
+version="$1"
+
+mvn versions:set -DnewVersion="$1"
+mvn versions:commit
+
+git add pom.xml
+git add */pom.xml
+
+commit_msg="release.sh $1"
+
+git commit -m "\"$commit_msg\""
+git push
+
+echo "Running maven deploy"
+
+mvn clean deploy
+
+echo "Released $version"
