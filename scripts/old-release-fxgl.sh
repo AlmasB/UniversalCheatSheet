632eaceb717f22c8c128813d73103488195ba986
Almas Baimagambetov <almaslvl@gmail.com>
2020-12-10 10:38:35 +0000

    updated release fxgl sh to new model

--- scripts/old-release-fxgl.sh
+++ scripts/old-release-fxgl.sh
@@ -3,17 +3,22 @@
 version="$1"
 
 function release_fxgl {
-    echo "Setting new version to $version"
-
     mvn versions:set -DnewVersion="$version"
+    mvn versions:commit
+
+    git add pom.xml
+    git add */pom.xml
+
+    commit_msg="release.sh $version"
+
+    git commit -m "\"$commit_msg\""
+    git push
 
     echo "Running maven deploy"
 
     mvn -T 12 clean deploy -DskipTests=true -Dlicense.skip=true -Dpmd.skip=true
 
     echo "Released $version"
-    
-    mvn versions:revert
 }
 
 function make_uber_jar {
@@ -28,7 +33,17 @@ function make_uber_jar {
 if [ "$version" == "" ]; then
     echo "Please provide release version"
 else
-    echo "Releasing..."
-    release_fxgl
-    make_uber_jar
+    # check if running on master branch
+
+    branch_name="$(git rev-parse --abbrev-ref HEAD)"
+
+    # check if any changes
+    if [ "$branch_name" == "master" ]; then
+        echo "Running on master branch"
+        release_fxgl
+        make_uber_jar
+        bump_version.sh dev
+    else
+        echo "FXGL can only be released from master branch"
+    fi
 fi
